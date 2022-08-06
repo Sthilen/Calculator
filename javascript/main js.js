ad4b74b1b8b044f0343b7ba6e3ec8e3a385c9379
Sthilen <110030158+Sthilen@users.noreply.github.com>
2022-08-06 17:35:53 +0700

    Update main js.js

--- javascript/main js.js
+++ javascript/main js.js
@@ -1,21 +1,16 @@
-// function trueOrFalse(wasThatTrue) {
-//     if (wasThatTrue) {
-//         return "It's true";
-//     }
-//     return "It's false";
-// };
-
-// console.log(trueOrFalse(true))
-
 var numberA = "";
+
+var numberAFinished = "";
 
 var numberB = "";
 
+operator = "";
+
 var result = 0;
 
-function button() {
-    switch(document.getElementsByClassName("numbers").value) {
-        case "1":
+function button(val) {
+    switch(val) {
+        case 1:
             numberA += "1";
             break;
         case 2:
@@ -42,26 +37,99 @@ function button() {
         case 9:
             numberA += "9";
             break;
+        default:
+            alert("no");
+            break;
     }
     return numberA
 }
 
-function math(operator) {
+function operators(operator) {
     switch(operator) {
-        case "+":
-            result = parseInt(numberA) + parseInt(numberB);
+        case '+':
+            numberAFinished = numberA;
+            numberB = "";
+            operator = "+"
+            alert(numberAFinished)
             break;
         case "-":
-            result = parseInt(numberA) - parseInt(numberB);
+            numberAFinished = numberA;
+            numberB = "";
+            operator = "-"
             break;
         case "*":
-            result = parseInt(numberA) * parseInt(numberB);
+            numberAFinished = numberA;
+            numberB = "";
+            operator = "*"
             break;
         case "/":
-            result = parseInt(numberA) / parseInt(numberB);
+            numberAFinished = numberA;
+            numberB = "";
+            operator = "/"
             break;
     }
-    return result;
+    return numberAFinished && numberB && operator;
 }
 
-console.log(numberA)
+function button(val) {
+    switch(val) {
+        case 1:
+            numberB += "1";
+            break;
+        case 2:
+            numberB += "2";
+            break;
+        case 3:
+            numberB += "3";
+            break;
+        case 4:
+            numberB += "4";
+            break;
+        case 5:
+            numberB += "5";
+            break;
+        case 6:
+            numberB += "6";
+            break;
+        case 7:
+            numberB += "7";
+            break;
+        case 8:
+            numberB += "8";
+            break;
+        case 9:
+            numberB += "9";
+            break;
+        default:
+            alert("no");
+            break;
+    }
+    return numberB
+}
+
+function results() {
+    switch(operator) {
+        case "+":
+            result= parseInt(numberAFinished) + parseInt(numberB);
+            alert(result);
+            break;
+        case "-":
+            result= parseInt(numberAFinished) - parseInt(numberB);
+            alert(result);
+            break;
+        case "*":
+            result= parseInt(numberAFinished) * parseInt(numberB);
+            alert(result);
+            break;
+        case "/":
+            result= parseInt(numberAFinished) / parseInt(numberB);
+            alert(result);
+            break;
+    }
+}
+
+function clear() {
+    numberA = "";
+    numberB = "";
+    operator = "";
+}
